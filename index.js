e827f6247daa5e7117fa7f04312667a27fa43842
Aletha Wheeless <augustkama98@gmail.com>
2022-05-07 18:03:31 -1000

    commit tree af4e22d6a1ef45cd4c6ffa95ab8164811bbb25a8

--- index.js
+++ index.js
@@ -12,5 +12,7 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  const splitArr = tutorials.map(title => title.split(' ').map(word => word[0].toUpperCase() + word.slice(1)))
+  const newArr = splitArr.map(arr => arr.join(' '))
+  return newArr;
 }
